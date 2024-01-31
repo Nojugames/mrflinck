ec8cfa728d2d9325f588b519dd44ee32663c3617
Nojugames <bjorn.zweigberg@gmail.com>
2024-01-31 15:17:41 +0200

    after launch fixes

--- header.php
+++ header.php
@@ -5,18 +5,7 @@
     <meta name="viewport" content="width=device-width"/>
     <!--<script src="https://kit.fontawesome.com/c5ba4af5bc.js" crossorigin="anonymous"></script>-->
 	<?php wp_head(); ?>
-    <script>
-        var prevScrollpos = window.pageYOffset;
-        window.onscroll = function() {
-            var currentScrollPos = window.pageYOffset;
-            if (prevScrollpos > currentScrollPos || currentScrollPos <= 100) {
-                document.getElementById("header").style.top = "0";
-            } else {
-                document.getElementById("header").style.top = "-80px";
-            }
-            prevScrollpos = currentScrollPos;
-        }
-    </script>
+
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/photoswipe/photoswipe.esm.min.js"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/photoswipe/photoswipe-lightbox.esm.min.js"></script>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/photoswipe/photoswipe.css">
